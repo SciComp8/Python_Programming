9a72cca2c6f1201f7814d1538251b1736a25b539
Anni <90419301+SciComp8@users.noreply.github.com>
2024-11-21 18:03:30 -0500

    Update if_elif_else.py

--- Utilities/Flow_Control/if_elif_else.py
+++ Utilities/Flow_Control/if_elif_else.py
@@ -3,7 +3,9 @@ for aa in amino_acids:
     if aa.find("Cystein", 0, 40) == -1:
         print("Cystein not found")
     #Count occurrences and replace two with one
-    elif aa.count("Cystein") == 2:  
+    elif aa.count("Cystein") == 1:
+        print(aa)
+    elif aa.count("Cystein") == 2:
         print(aa.replace("Cystein Cystein", "Cystein"))
     else:
         print(aa.replace("Cystein Cystein Cystein", "Cystein")) #Replace three occurrences with one
