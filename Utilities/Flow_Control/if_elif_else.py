78313f610e07e86eb685b9bd6634478e21a38e39
Anni <90419301+SciComp8@users.noreply.github.com>
2024-11-21 17:52:11 -0500

    Update if_elif_else.py

--- Utilities/Flow_Control/if_elif_else.py
+++ Utilities/Flow_Control/if_elif_else.py
@@ -1,10 +1,11 @@
 for aa in amino_acids:
-  	# If Cystein is not found between character 20 and 40 inclusive, print Cystein not found
+    #If Cystein is not found between character 20 and 40 inclusive, print Cystein not found
     if aa.find("Cystein", 20, 40) == -1:
         print("Cystein not found")
-    # Count occurrences and replace two with one
-    elif aa.count("actor") == 2:  
+    #Count occurrences and replace two with one
+    elif aa.count("Cystein") == 2:  
         print(aa.replace("Cystein Cystein", "Cystein"))
     else:
-        # Replace three occurrences with one
-        print(aa.replace("Cystein Cystein Cystein", "Cystein"))
+        print(aa.replace("Cystein Cystein Cystein", "Cystein")) #Replace three occurrences with one
+
+aa = "Cystein Cystein Cystein"
