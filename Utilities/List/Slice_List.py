952b59f860dc13a59934ececaf5a8e70e6995d62
Anni <90419301+SciComp8@users.noreply.github.com>
2025-01-11 12:12:00 -0500

    Create Slice_List.py

---
+++ Utilities/List/Slice_List.py
@@ -0,0 +1,11 @@
+social_science_data = [
+    ["respondent_id", "age", "gender", "education_level", "income", "employment_status", "marital_status", "survey_score"],
+    [1, 25, "Female", "Bachelor's", 45000, "Employed", "Single", 78],
+    [2, 34, "Male", "Master's", 60000, "Employed", "Married", 85],
+    [3, 29, "Non-binary", "High School", 32000, "Unemployed", "Single", 65],
+    [4, 45, "Female", "PhD", 75000, "Employed", "Married", 90],
+    [5, 51, "Male", "Bachelor's", 50000, "Retired", "Divorced", 70],
+]
+
+for row in social_science_data:
+    print(row)
