e6bfc4555ed1f319277d56af36025d236817db36
Anni <90419301+SciComp8@users.noreply.github.com>
2025-01-19 11:17:59 -0500

    Update List_Basics.py

--- Utilities/List/List_Basics.py
+++ Utilities/List/List_Basics.py
@@ -1,2 +1,10 @@
 # List is an ordered sequences of objects
 # It can take a mix of different types of objects
+# It is mutable
+
+city = ['Tokyo', 'Shanghai', 'New York City', 'Los Angeles', 'Paris', 'London']
+city[0] = 'Cairo' 
+city[0:2] = ['Tokyo', 'Chicago']
+
+# View the length of list
+len(city)
