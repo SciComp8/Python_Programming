b8ca45a13796e08faac5f69d9194b56b136589dd
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-01-26 15:38:44 -0500

    Update pandas_create_save.py

--- Utilities/pandas/pandas_create_save.py
+++ Utilities/pandas/pandas_create_save.py
@@ -1,4 +1,5 @@
 # Initialize an empty DataFrame
+import pandas as pd
 df = pd.DataFrame()
 submission_df['label'] = predictions # predictions is a pandas series
 
@@ -6,22 +7,34 @@ submission_df['label'] = predictions # predictions is a pandas series
 my_dict = {"key_1": val_1, "key_2": val_2, "key_3", val_3} 
 my_dict["key_1] # Access values of a dictionary 
 
-# Create DataFrames from a list of dictionaries	- construct the df row by row
+"""
+Create DataFrames from a list of dictionaries	- construct the df row by row
+"""
 # Link: Convert_List_to_Dictionary.py
 list_of_dict = [{"col_1": val_11, "col_2": val_21"}, {"col_1": val_12, "col_2": val_22"}]
 my_df = pd.DataFrame(list_of_dict)
 print(my_df)
 
-# ***Create DataFrames from a dictionary of lists - construct the df column by column 
+"""
+***Create DataFrames from a dictionary of lists - construct the df *column by column* 
+"""
 dict_of_list = {"col_1": [val_11, val_12], "col_2": [val_21, val_22]}
 my_df = pd.DataFrame(list_of_dict)
 print(my_df)
 
-# Horizontally concatenate a new DataFrame to the old DataFrame
+"""
+Create DataFrames from a dictionary
+out_df = pd.DataFrame({'col_name_1': test_data.var_name_1, 'col_name_2': test_data.var_name_2})
+"""
+
+"""
+Horizontally concatenate a new DataFrame to the old DataFrame
+"""
 df_whole = pd.concat([df_old, df_new])
 
-# Share DataFrames in the comma-separated values files
-import pandas as pd
+"""
+Share DataFrames in the comma-separated values files
+"""
 df = pd.read_csv("data.csv")  
 print(df)
 
