1f3449f838c6588500cccd01c4fc8a32a0aceb62
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-01-28 17:07:37 -0500

    Update Decision_Tree_Price.py

--- Artificial_Intelligence/Machine_Learning/Decision_Tree_Price.py
+++ Artificial_Intelligence/Machine_Learning/Decision_Tree_Price.py
@@ -1,6 +1,7 @@
 # Set up coding environment
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
 
 file_path = '../raw/sale_data/train.csv'
 
@@ -18,12 +19,19 @@ X = sale_data[feature_names]
 print(X.describe())
 print(X.head())
 
-# 3: specify and fit model
+# 3: Split data into training and validation data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=9)
+
+# 4: specify and fit model
 sale_model = DecisionTreeRegressor(random_state=9)
 sale_model.fit(X, y)
 
-# 4: make predictions
-predictions = sale_model.predict(X)
-print(predictions)
-print(y.head())
-print(predictions[0:5])
+# 5: make predictions
+val_predictions = sale_model.predict(val_X)
+print(val_y.head())
+print(val_predictions[0:5])
+
+# 5: evaluate model
+print(mean_absolute_error(val_y, val_predictions))
+
+
