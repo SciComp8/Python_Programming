88295655dc891a57024f982cbb05210b9be026a4
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-01-29 17:38:45 -0500

    Update Random_Forest_Price.py

--- Artificial_Intelligence/Machine_Learning/Random_Forest_Price.py
+++ Artificial_Intelligence/Machine_Learning/Random_Forest_Price.py
@@ -1,3 +1,8 @@
 # Reference: https://www.youtube.com/watch?v=2_iKR_fDKNM | NISS AI, Statistics & Data Science in Practice: Lucas Mentch - Random Forests
 # mtry defines the number of variables randomly sampled as candidates at each split
 # From the standpoint of model complexity, this parameter in random forests functions similarly to the shrinkage penalty used in explicit regularization methods such as the lasso.
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
