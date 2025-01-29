ab0cf76a1c8a83e697b0494e28eb3d216cc69d5e
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-01-29 18:03:28 -0500

    Create Create_SymbolicLink.py

---
+++ Utilities/File/Create_SymbolicLink.py
@@ -0,0 +1,4 @@
+import os
+if not os.path.exists("../raw/data/train.csv"):
+    os.symlink(source="../Downloads/train.csv", destination="../input/train.csv") # Create a symlink at "../raw/data/train.csv" that points to "../Downloads/train.csv"
+    os.symlink(source="../Downloads/test.csv", destination="../input/test.csv") 
