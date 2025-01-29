6730f1fd2186e8b855687526837422cab321e5e1
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-01-29 17:36:28 -0500

    Create Random_Forest_Price.py

---
+++ Artificial_Intelligence/Machine_Learning/Random_Forest_Price.py
@@ -0,0 +1,3 @@
+# mtry defines the number of variables randomly sampled as candidates at each split
+# From the standpoint of model complexity, this parameter in random forests functions similarly to the shrinkage penalty used in explicit regularization methods such as the lasso.
+# 
