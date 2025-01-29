e6550de1e3b1f549eb2f4c4c74ea92b231cdcc24
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-01-29 18:29:03 -0500

    Update One_Hot_Encoding.py

--- Artificial_Intelligence/Machine_Learning/One_Hot_Encoding.py
+++ Artificial_Intelligence/Machine_Learning/One_Hot_Encoding.py
@@ -1,1 +1,8 @@
-# 
+import pandas as pd
+train_data = pd.read_csv("../data/raw/train.csv")
+test_data = pd.read_csv("../data/raw/test.csv")
+
+y = train_data["Outcome"]
+features = ["Gender", "Ethnicity"]
+X = pd.get_dummies(train_data[features]) 
+X_test = pd.get_dummies(test_data[features]) #
