e8dd1922e77c054779d8a1a0c43fe1aaf6e4536c
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-01-29 17:42:39 -0500

    Update Random_Forest_Price.py

--- Artificial_Intelligence/Machine_Learning/Random_Forest_Price.py
+++ Artificial_Intelligence/Machine_Learning/Random_Forest_Price.py
@@ -6,3 +6,37 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
+
+# 0: Set up environment
+file_path = '../raw/sale_data/train.csv'
+sale_data = pd.read_csv(file_path)
+
+# Drop rows with missing values
+sale_data = sale_data.dropna(axis=0)
+
+# 1: Specify prediction target
+sale_data.columns
+y = sale_data.Price
+
+# 2: Create feature sets
+feature_names = ['Area', 'Year', 'GDP', 'Population']
+X = sale_data[feature_names]
+
+# Review data
+print(X.describe())
+print(X.head())
+
+# 3: Split data into training and validation data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=9)
+
+# 4: Specify the model and fit the model with training data
+sale_model_rf = RandomForestRegressor(random_state=9)
+sale_model_rf.fit(train_X, train_y)
+
+# 5: Make predictions
+val_predictions = sale_model_rf.predict(val_X)
+print(val_y.head().tolist())
+print(val_predictions[0:5])
+
+# 6: Evaluate the model
+print(mean_absolute_error(val_y, val_predictions))
