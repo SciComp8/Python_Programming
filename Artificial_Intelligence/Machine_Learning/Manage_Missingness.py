4016c71709e79e6dfe7ed3c8137aac284b086a1f
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-02-04 10:25:18 -0500

    Update Manage_Missingness.py

--- Artificial_Intelligence/Machine_Learning/Manage_Missingness.py
+++ Artificial_Intelligence/Machine_Learning/Manage_Missingness.py
@@ -4,11 +4,16 @@
 df_clean = df.dropna(axis=1, how='any')
 # how='all': drops a column only if all values are missing
 
-# 2. Drop a column with missing values when the missingness proportion in this column is very high.
+df_clean = df.dropna(axis=1, how='any').copy()
+# If I modify df_clean without altering df, then using .copy() is a safe practice. 
+# If I only read or perform operations that don’t change df_clean, I will not need .copy().
+
+# 2. Drop a column with missing values when the missingness proportion in this column is very high
 
 # 3. Impute the missing values using median or mean
 miss_median = lambda x: x.fillna(x.median())
 df["column_name_1"].transform(miss_median)
+
 miss_mean = lambda x: x.fillna(x.mean())
 df["column_name_1"].transform(miss_mean)
 
