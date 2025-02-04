4c95ce68124dee57a3b68ca4bb9c9c38f42ad7f9
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-02-03 19:58:33 -0500

    Update Deep_Copy.py

--- Utilities/pandas/Deep_Copy.py
+++ Utilities/pandas/Deep_Copy.py
@@ -10,4 +10,12 @@ print(gene_count) # Original remains unchanged
 gene_count_shallow_copy = gene_count # Shallow copy
 gene_count_shallow_copy['Gene_1'][0] = 7  
 print(gene_count_shallow_copy)
-print(gene_count) # Original remains CHANGED
+print(gene_count) # Original remains CHANGED!
+
+###
+region_count = pd.DataFrame({'Region_1': [[5, 10], [8, 6]]})
+region_count_deep_copy = region_count.copy() 
+
+region_count_deep_copy['Region_1'][0].append(8)  # Modify the list in the copy
+print(region_count_deep_copy['Region_1'][0])
+print(region_count['Region_1'][0]) # Original is also CHANGED!
