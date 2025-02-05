c5484ec13d305198bd72f1157b38c7c10b163e42
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-02-05 11:23:16 -0500

    Update One_Hot_Encoding.py

--- Artificial_Intelligence/Machine_Learning/One_Hot_Encoding.py
+++ Artificial_Intelligence/Machine_Learning/One_Hot_Encoding.py
@@ -11,11 +11,11 @@ y_train = data_train["Methylation_Level"]
 y_valid = data_valid["Methylation_Level"]
 features = ["Grade_Category", "Age_Category", "BMI_Category"]
 
-# Method 1: 
+### Method 1: 
 X_train = pd.get_dummies(data_train[features]) 
 X_test = pd.get_dummies(data_test[features]) #
 
-# Method 2: 
+### Method 2: 
 OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
 # handle_unknown='ignore': prevent errors when the validation set contains classes absent from the training data
 # sparse=False: guarantees that the encoded columns are output as a dense NumPy array rather than as a sparse matrix
@@ -26,9 +26,14 @@ X_valid_OH.index = X_valid.index
 
 # Select numerical variables
 X_train_num = data_train.drop(features, axis=1)
+# data_train.dtypes
+# X_train_num = data_train.select_dtypes(exclude=['object'])
 X_valid_num = data_valid.drop(features, axis=1)
 
 # Combine numerical variables and one-hot encoded variables
 X_train = pd.concat([X_train_num, X_train_OH], axis=1)
 X_valid = pd.concat([X_valid_num, X_valid_OH], axis=1)
+
+X_train.columns = X_train.columns.astype(str) # Convert each column name in the index to a string data type
+X_valid.columns = X_valid.columns.astype(str)
 
