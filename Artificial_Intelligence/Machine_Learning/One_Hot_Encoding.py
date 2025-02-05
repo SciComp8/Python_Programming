dbf18e5d8e01c69ac693d3c8b30a9ab3ab75edb3
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-02-05 09:31:15 -0500

    Update One_Hot_Encoding.py

--- Artificial_Intelligence/Machine_Learning/One_Hot_Encoding.py
+++ Artificial_Intelligence/Machine_Learning/One_Hot_Encoding.py
@@ -1,3 +1,6 @@
+# The one-hot encoding approach is suitable when the categorical variable does not show clear ordering.
+# But, when the categorical variable takes on > 15 different values, this approach does not work well.
+
 import pandas as pd
 train_data = pd.read_csv("../data/raw/train.csv")
 test_data = pd.read_csv("../data/raw/test.csv")
