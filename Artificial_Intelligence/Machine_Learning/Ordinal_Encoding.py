7c5a77bb1a49a54b995879bda8df5e47a7e60413
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-02-05 09:37:13 -0500

    Update Ordinal_Encoding.py

--- Artificial_Intelligence/Machine_Learning/Ordinal_Encoding.py
+++ Artificial_Intelligence/Machine_Learning/Ordinal_Encoding.py
@@ -13,6 +13,5 @@ ordinal_encoder = OrdinalEncoder()
 
 X_train = ordinal_encoder.fit_transform(data_train[features]) 
 X_valid = ordinal_encoder.transform(data_valid[features]) #
-
 # We apply the same ordinal encoding parameters (categorical integer value) that were derived from the training data, without recalculating them on the validation data. 
 # This ensures that the variable encoding process remains consistent and that the validation set doesn't influence the encoding statistics.
