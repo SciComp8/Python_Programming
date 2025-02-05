5a28271a4f4b4f551ae6877262be35cb2871ad7c
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-02-05 11:23:49 -0500

    Update Ordinal_Encoding.py

--- Artificial_Intelligence/Machine_Learning/Ordinal_Encoding.py
+++ Artificial_Intelligence/Machine_Learning/Ordinal_Encoding.py
@@ -5,6 +5,13 @@ from sklearn.preprocessing import OrdinalEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 
+# Define a scoring mechanism to evaluate the impact of each encoding strategy on model performance
+def score_encode_strategy(X_t=X_train, X_v=X_valid, y_t=y_train, y_v=y_valid):
+    rf = RandomForestRegressor(n_estimators=50, random_state=9)
+    rf.fit(X_t, y_t)
+    preds = rf.predict(X_v)
+    return mean_absolute_error(y_v, preds)
+
 data_train = pd.read_csv("../data/raw/train.csv")
 data_valid = pd.read_csv("../data/raw/valid.csv")
 
@@ -20,13 +27,6 @@ X_valid = ordinal_encoder.transform(data_valid[features]) #
 # However, we may achieve better performance by assigning more meaningful, informed labels to ordinal variables.
 # We apply the same ordinal encoding parameters (categorical integer value) that were derived from the training data, without recalculating them on the validation data. 
 # This ensures that the variable encoding process remains consistent and that the validation set doesn't influence the encoding statistics.
-
-# Define a scoring mechanism to evaluate the impact of each encoding strategy on model performance
-def score_encode_strategy(X_t=X_train, X_v=X_valid, y_t=y_train, y_v=y_valid):
-    rf = RandomForestRegressor(n_estimators=50, random_state=9)
-    rf.fit(X_t, y_t)
-    preds = rf.predict(X_v)
-    return mean_absolute_error(y_v, preds)
 
 print("The mean absolute error from Ordinal Encoding:") 
 print(score_encode_strategy(X_train, X_valid, y_train, y_valid))
