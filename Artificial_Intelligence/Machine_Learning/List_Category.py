858739216c8d179e0b2389e2c1a70c018e566a3a
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-02-06 17:40:58 -0500

    Update List_Category.py

--- Artificial_Intelligence/Machine_Learning/List_Category.py
+++ Artificial_Intelligence/Machine_Learning/List_Category.py
@@ -1,9 +1,16 @@
 # Reference:
 # https://github.com/SciComp8/Python_Programming/blob/main/Utilities/List/*List_Comprehension.py
+# https://github.com/SciComp8/Python_Programming/blob/main/Utilities/Function/Lambda_Function.py
 
 import pandas as pd
 X_train = pd.read_csv('../data/raw/training.csv')
-cat_var = [col for col in X_train.columns if X_train[col].dtype == "object"]
+cat_var_name = [col for col in X_train.columns if X_train[col].dtype == "object"]
 
 # Obtain the number of unique categories in each categorical variable
-cat_var_nunique = list(map(lambda col: X_train[col].nunique(), cat_var))
+cat_var_nunique = list(map(lambda col: X_train[col].nunique(), cat_var_name))
+cat_var_nunique_d = dict(zip(cat_var_name, cat_var_nunique))
+print(cat_var_nunique_d)
+
+# Show the number of unique categories by categorical variable, in ascending order
+sorted(cat_var_nunique_d.items(), key=lambda x: x[1])
+
