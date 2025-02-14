f3a4bd0b06328ff2b33d70df2e839dc65c66eac3
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-02-13 20:56:12 -0500

    Update Pipeline.py

--- Artificial_Intelligence/Machine_Learning/Pipeline.py
+++ Artificial_Intelligence/Machine_Learning/Pipeline.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 
