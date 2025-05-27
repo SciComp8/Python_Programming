4b4ebab8fbb9a21e426d7ed411ca073043d84d59
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-05-27 12:46:41 -0400

    Update Read_Save_CSV.py

--- Utilities/pandas/Read_Save_CSV.py
+++ Utilities/pandas/Read_Save_CSV.py
@@ -1,5 +1,6 @@
 # Reference
 # https://towardsdatascience.com/its-time-to-say-goodbye-to-pd-read-csv-and-pd-to-csv-27fbc74e84c5/
+
 # For datasets with fewer than 1 million rows, Pandas and Dask exhibited similar performance in reading CSV files.
 # As the dataset size increased beyond 1 million rows, Dask's performance declined, taking significantly more time than Pandas to read the CSV files.
 # DataTable consistently outperformed both, reading CSV files 4 to 5 times faster than Pandas, making it the most efficient among the three for this task.
