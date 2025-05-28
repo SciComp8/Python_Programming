cfa45e0368ea89e4223893e3cf7882998d14ecc0
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-05-28 16:26:01 -0400

    Update Create_Class_v1.py

--- Utilities/Class/Create_Class_v1.py
+++ Utilities/Class/Create_Class_v1.py
@@ -43,7 +43,9 @@ print(f"Max Expression: {max_expression}")
 dir(rna_sample) # List all the attributes and methods of an object
 help(rna_sample) # Show the documentation of a class associated with an object
 
-# Define attributes in the __init__ constructor; avoid defining attributes outside the constructor, making it easier for collaborators to locate all attributes
+# Define attributes in the __init__ constructor
+# Why build __init__ constructor? 
+# Because we want to avoid defining attributes outside the constructor, making it easier for collaborators to locate all attributes
 class RNASample2:
     def __init__(self, new_name, values=None):
         self.name = new_name
