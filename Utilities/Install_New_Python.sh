970441443ce119510f6bc39dee41570c40a6a365
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-06-09 20:22:12 -0400

    Update Install_New_Python.sh

--- Utilities/Install_New_Python.sh
+++ Utilities/Install_New_Python.sh
@@ -6,6 +6,7 @@ brew link --overwrite python@3.10
 # Ref: https://github.com/pyenv/pyenv?tab=readme-ov-file#switch-between-python-versions
 pyenv install --list | grep "3.10" # List available Python versions
 pyenv uninstall 3.8.18
+rm ~/.python-version # Delete the file forcing pyenv to use 3.8.18
 pyenv install 3.10.9
 pyenv global 3.10.13 # Set it as the global version
 python --version 
