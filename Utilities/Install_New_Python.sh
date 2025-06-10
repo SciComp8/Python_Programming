25787326e1e696bc8370ecb1935f7553ba8c503d
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-06-09 20:08:50 -0400

    Update Install_New_Python.sh

--- Utilities/Install_New_Python.sh
+++ Utilities/Install_New_Python.sh
@@ -1,2 +1,10 @@
+# Method 1
 brew install python@3.10
 brew link --overwrite python@3.10
+
+# Method 2
+# Ref: https://github.com/pyenv/pyenv?tab=readme-ov-file#switch-between-python-versions
+pyenv install --list | grep "3.10" # List available Python versions
+pyenv install 3.10.9
+pyenv global 3.10.13 # Set it as the global version
+python --version 
